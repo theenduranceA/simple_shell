06a273e7fa89464a5bebd4acf84d073350daf328
theenduranceA <theenduranceaneke@gmail.com>
2023-05-15 13:43:59 -0700

    added

--- shell.h
+++ shell.h
@@ -10,8 +10,10 @@
 #include <sys/stat.h>
 #include <sys/wait.h>
 
-int main(int ac, char **av);
-int _getline(char **lineptr, size_t n, FILE *stream);
+int main(void);
+char *my_strtok(char *str, const char *delim);
+void my_input(char *input);
+ssize_t getline(char **my_lineptr, size_t *n, FILE *stream);
 int _atoi(char *s);
 int _isdigit(char *s);
 int _putchar(char c);
@@ -20,7 +22,6 @@ int _strlen(char *s);
 int *_strdup(char *str);
 char _strcmp(char *s1, char *s2);
 char *_strcat(char *dest, char *src);
-char *_strtok(char *str, const char *delim);
 char *_strcpy(char *dest, const char *src);
 
 #endif
