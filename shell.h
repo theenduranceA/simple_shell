5fb210b8d26568f97e0fae92aae6a56968a96c20
Jonah <jonahemmanuel54@gmail.com>
2023-05-18 01:18:35 -0700

    simple_shell

--- shell.h
+++ shell.h
@@ -1,27 +1,63 @@
-#ifndef SHELL_H
-#define SHELL_H
+#ifndef _SHELL_
+#define _SHELL_
 
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <fcntl.h>
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <sys/types.h>
-#include <sys/stat.h>
 #include <sys/wait.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <stdarg.h>
+#include <signal.h>
+#define PROMPT "$: "
 
-int main(void);
-char *my_strtok(char *str, const char *delim);
-void my_input(char *input);
-ssize_t getline(char **my_lineptr, size_t *n, FILE *stream);
-int _atoi(char *s);
-int _isdigit(char *s);
-int _putchar(char c);
-int _puts(char *s);
-int _strlen(char *s);
-int *_strdup(char *str);
-char _strcmp(char *s1, char *s2);
-char *_strcat(char *dest, char *src);
-char *_strcpy(char *dest, const char *src);
+
+/**
+ * struct builtin_d - Defines the builtins functions.
+ * @built: The name of the build in command.
+ * @f: A pointer to the right builtin function.
+ */
+typedef struct builtin_d
+{
+	char *built;
+	void (*f)(char *);
+} builtin_t;
+
+extern char **environ;
+
+char **token_interface(char *, const char *, int);
+int count_token(char *, const char *);
+char **tokenize(int, char *, const char *);
+void create_child(char **, char *, int, char **);
+void parse_line(char *, size_t, int, char **);
+char *path_finder(char *);
+int str_len(char *);
+int find_path(char *);
+char **tokenize_path(int, char *);
+char *search_directories(char **, char *);
+char *build_path(char *, char *);
+void double_free(char **);
+void single_free(int, ...);
+
+/*Builtin functions*/
+int built_in(char **, char *);
+void (*check_built_ins(char *))(char *);
+void u_exit(char *);
+void _env(char *);
+void _cd(char *);
+
+/*custom library functions*/
+int _strcmp(char *, char *);
+char *_strdup(char *);
+void print_str(char *, int);
+int print_number(int);
+int _write_char(char);
+
+/* Helper functions*/
+void error_printing(char *, int, char *);
+void exec_error(char *, int, char *);
 
 #endif
